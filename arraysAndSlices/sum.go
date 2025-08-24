4cda208b8db8f460b54f4538aecce3cdbca2c78a
jdav892 <jadavila9@gmail.com>
2025-08-24 18:28:54 -0400

    feat: fixes function that should not have passed test

--- arraysAndSlices/sum.go
+++ arraysAndSlices/sum.go
@@ -1,9 +1,9 @@
 package arraysandslices 
 
-func Sum(Numbers [5]int) int {
+func Sum(numbers [5]int) int {
 	count := 0
-	for i := 0; i <= 5 ; i++ {
-		count += i	
+	for i := 0; i < 5 ; i++ {
+		count += numbers[i]
 	}
 	return count
 
