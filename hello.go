e0ae73711346b323c0de252f5343ee9bc059c012
jdav892 <jadavila9@gmail.com>
2025-08-24 01:39:56 -0400

    feat: added another function and test for it

--- hello.go
+++ hello.go
@@ -3,24 +3,35 @@ package main
 import "fmt"
 
 const (
+		citizen = "Citizen"
+
 		french = "French"
  		spanish = "Spanish"
  		japanese = "Japanese"
+		
+		earthling = " of Earth"
+		america = " from America"
+		asia = " from Asia"
+		europe = " from Europe"
+
+		france = "France"
+		spain = "Spain"
+		japan = "Japan"
+		usa = "USA"
 
  		englishHelloPrefix = "Hello, "
  		frenchHelloPrefix = "Bonjour, "
  		japaneseHelloPrefix = "Konichiwa, "
  		spanishHelloPrefix = "Hola, " 
 )
-func Hello(name string, language string) string {
+func Hello(name string, language string, country string) string {
 	if name == "" {
-		name = "World"
+		name = citizen
 	}
-	
-	return greetingPrefix(language) + name
 
+	return greetingPrefix(language) + name + personalOrigin(country)
 }
-
+//public functions have capital letters while private functions have lowercase letters
 func greetingPrefix(language string)(prefix string) {
 	switch language {
 	case spanish:
@@ -35,8 +46,24 @@ func greetingPrefix(language string)(prefix string) {
 	return
 }
 
+func personalOrigin(country string)(origin string) {
+	switch country {
+	case spain:
+		origin = europe
+	case france:
+		origin = europe
+	case japan:
+		origin = asia
+	case usa:
+		origin = america
+	default:
+		origin = earthling
+	}
+	return
+}
+
 func main() {
-	fmt.Println(Hello("world", ""))
+	fmt.Println(Hello("world", "", ""))
 }
 
 
--- hello_test.go
+++ hello_test.go
@@ -3,30 +3,30 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	//example of subtests
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Justin", "")
-		want := "Hello, Justin"
+//example of subtests
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("", "", "")
+		want := "Hello, Citizen of Earth"
 		assertCorrectMessage(t, got, want)
 	})
-	t.Run("empty string defaults to 'world'", func(t *testing.T) {
-		got := Hello("", "")
-		want := "Hello, World"
+	t.Run("in English", func(t *testing.T) {
+		got := Hello("Justin", "English", "USA")
+		want := "Hello, Justin from America"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("in Spanish", func(t *testing.T) {
-		got := Hello("Elodie", "Spanish")
-		want := "Hola, Elodie"
+		got := Hello("Elodie", "Spanish", "Spain")
+		want := "Hola, Elodie from Europe"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("in French", func(t *testing.T) {
-		got := Hello("Pierre", "French")
-		want := "Bonjour, Pierre"
+		got := Hello("Pierre", "French", "France")
+		want := "Bonjour, Pierre from Europe"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("in Japanese", func(t *testing.T) {
-		got := Hello("Akira", "Japanese")
-		want := "Konichiwa, Akira"
+		got := Hello("Akira", "Japanese", "Japan")
+		want := "Konichiwa, Akira from Asia"
 		assertCorrectMessage(t, got, want)
 	})
 }
