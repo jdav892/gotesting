1938879457d1cf5edaa615fe61c52decd4b60d5b
jdav892 <jadavila9@gmail.com>
2025-08-24 00:46:05 -0400

    feat: add assert helper

--- hello_test.go
+++ hello_test.go
@@ -3,20 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
+	//example of subtests
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Justin")
 		want := "Hello, Justin"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
-	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+
+//woah actually using asserts
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
