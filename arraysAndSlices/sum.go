27f22737fcbb124d08f27a1a87d139c78838a190
jdav892 <jadavila9@gmail.com>
2025-08-25 18:58:45 -0400

    feat: create tests to deal with multiple slice cases and refactor function

--- arraysAndSlices/sum.go
+++ arraysAndSlices/sum.go
@@ -19,8 +19,12 @@ func SumAll(numbersToSum ...[]int) []int {
 func SumAllTails(tailsToSum ...[]int) []int {
 	var sums []int
 	for _, numbers := range tailsToSum {
-		tail := numbers[1:]
-		sums = append(sums, Sum(tail))
+		if len(numbers) == 0{
+			sums = append(sums, 0)
+		}else{
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
 	return sums
 }
