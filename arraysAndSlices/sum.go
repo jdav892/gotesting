154e42d631e4c7870969caffb8b0fd838c69d492
jdav892 <jadavila9@gmail.com>
2025-08-25 03:10:50 -0400

    feat: uses the built in range to iterate over the given array, which still satisfies the test

--- arraysAndSlices/sum.go
+++ arraysAndSlices/sum.go
@@ -2,8 +2,8 @@ package arraysandslices
 
 func Sum(numbers [5]int) int {
 	count := 0
-	for i := 0; i < 5 ; i++ {
-		count += numbers[i]
+	for _, number := range numbers {
+		count += number
 	}
 	return count
 
