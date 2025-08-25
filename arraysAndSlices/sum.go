aefedd03930cfaf69665516bf8c26f873f3576b4
jdav892 <jadavila9@gmail.com>
2025-08-25 18:53:27 -0400

    feat: create function and test to sum all tail values in slices

--- arraysAndSlices/sum.go
+++ arraysAndSlices/sum.go
@@ -15,3 +15,12 @@ func SumAll(numbersToSum ...[]int) []int {
 	}
 	return sums
 }
+
+func SumAllTails(tailsToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range tailsToSum {
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
+	}
+	return sums
+}
