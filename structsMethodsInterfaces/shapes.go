2302f1f23bc530447c7c97ef5d98406d93c27769
jdav892 <jadavila9@gmail.com>
2025-08-27 23:17:41 -0400

    feat: create working test and function for area of a rectangle

--- structsMethodsInterfaces/shapes.go
+++ structsMethodsInterfaces/shapes.go
@@ -3,3 +3,7 @@ package structsMethodsInterfaces
 func Perimeter(width float64, height float64) float64 {
 	return 2 * (width + height)
 }
+
+func Area(length float64, width float64) float64 {
+	return (length * width)
+}
