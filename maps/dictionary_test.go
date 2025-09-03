cdf7bcebbe93c4a7fd6faa089b074704f4573467
Justin Davila <Jadavila9@gmail.com>
2025-09-03 05:26:01 -0400

    feat: progress towards finishing maps section

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -13,18 +13,29 @@ func TestSearch(t *testing.T) {
 	})
 
 	t.Run("unknown word", func(t *testing.T) {
-		_, err := dictionary.Search("unknown")
-		want := "could not find word"
-		
-		if err == nil {
+		_, got := dictionary.Search("unknown")
+		if got == nil {
 			t.Fatal("expected to get an error")
 		}
-		assertStrings(t, err.Error(), want)
+		assertStrings(t, got, ErrNotFound)
 	})
 
 }
 
-func assertStrings(t testing.TB, got, want string) {
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
+
+	want := "this is justa  test"
+	got, err := dictionary.Search("test")
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+	assertStrings(t, got, want)
+}
+
+
+func assertStrings(t testing.TB, got, want error) {
 	t.Helper()
 
 	if got != want {
