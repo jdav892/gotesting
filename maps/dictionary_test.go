36c2ccb5cb4b592ec03585b0b9d7cff47d1bf7e2
Justin Davila <Jadavila9@gmail.com>
2025-09-03 23:49:05 -0400

    feat: finish function to create dictionary with add method as well as its test

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,39 +3,69 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := Dictionary{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "testing"}
 
 	t.Run("known word", func(t *testing.T) {
 		got, _ := dictionary.Search("test")
-		want := "this is just a test"
+		want := "testing"
 
 		assertStrings(t, got, want)
 	})
 
 	t.Run("unknown word", func(t *testing.T) {
-		_, got := dictionary.Search("unknown")
+		_,got := dictionary.Search("unknown")
 		if got == nil {
 			t.Fatal("expected to get an error")
 		}
-		assertStrings(t, got, ErrNotFound)
+		assertError(t, got, ErrNotFound)
 	})
 
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	dictionary.Add("test", "this is just a test")
 
-	want := "this is justa  test"
-	got, err := dictionary.Search("test")
+	t.Run("new word", func(t *testing.T){ 
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "testing"
+
+		err := dictionary.Add(word, definition)
+		
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T){
+		word := "test"
+		definition := "testing"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+		
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
 	if err != nil {
 		t.Fatal("should find added word:", err)
 	}
-	assertStrings(t, got, want)
+	assertStrings(t, got, definition)
 }
 
-
-func assertStrings(t testing.TB, got, want error) {
+func assertError(t testing.TB, got, want error) {
 	t.Helper()
 
 	if got != want {
