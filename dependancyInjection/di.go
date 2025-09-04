c53a42867c4b1451ef7f2c446fc88585df11e14b
Justin Davila <Jadavila9@gmail.com>
2025-09-04 19:00:58 -0400

    feat: create handler to write to greet function using io log and net/http packages

--- dependancyInjection/di.go
+++ dependancyInjection/di.go
@@ -1,11 +1,19 @@
 package dependancyInjection
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
-func Greet(writer *bytes.Buffer, name string) {
+func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
 
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
 
+func main() {
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+}
