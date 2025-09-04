87433b6587aaa89d17b0281ef4283d140c2a4316
Justin Davila <Jadavila9@gmail.com>
2025-09-04 00:20:31 -0400

    feat: create function to update word if it it exists

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -5,6 +5,7 @@ type Dictionary map[string]string
 const (
 	ErrNotFound = DictionaryErr("could not find the word you were looking for")
 	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot perform operation on word because it does not exist")
 )
 
 type DictionaryErr string
@@ -36,6 +37,16 @@ func (e DictionaryErr) Error() string {
 	return string(e)
 }
 
-func (d Dictionary) Update(word, definition string) {
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
 	d[word] = definition
+	default:
+		return err
+	}
+	return nil
 }
