5b9c47ebe0edf33bfde9568e0d88a6afb0700a91
Justin Davila <Jadavila9@gmail.com>
2025-09-07 23:41:47 -0400

    feat: create sleeper and spy for the function to see how often its called and test for them

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -7,20 +7,42 @@ import(
 	"os"
 )
 
+type DefaultSleeper struct {
+
+}
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+//add new line character to go string for correct terminal output with this function
 const finalWord = "Go!"
 const countDown = 3
 
-func Countdown(out io.Writer){
+func Countdown(out io.Writer, sleeper Sleeper){
 	//using for loop to decrement and recreate a countdown
 	for i:= countDown; i > 0; i-- {
 		fmt.Fprintln(out, i)
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 	}
 	fmt.Fprint(out, finalWord)
 }
 
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
 
