73122e53ed182baf7cad244903776f3472bd5d13
Justin Davila <Jadavila9@gmail.com>
2025-09-10 04:09:52 -0400

    feat: implement reflection and revert countdown function

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -19,18 +19,21 @@ type SpySleeper struct {
 	Calls int
 }
 
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
 //add new line character to go string for correct terminal output with this function
 const finalWord = "Go!"
 const countDownStart = 3
+const write = "write"
+const sleep = "sleep"
 
 func Countdown(out io.Writer, sleeper Sleeper){
 	//using for loop to decrement and recreate a countdown
 	for i := countDownStart; i > 0; i-- {
-		sleeper.Sleep()
-	}
-
-	for i := countDownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
+		sleeper.Sleep()
 	}
 	fmt.Fprint(out, finalWord)
 }
@@ -41,6 +44,15 @@ func (d *DefaultSleeper) Sleep() {
 
 func (s *SpySleeper) Sleep() {
 	s.Calls++
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
 }
 
 
