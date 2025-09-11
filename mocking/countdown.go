c72acc5d6bd91f5748f4626a0d9b99d8afb15c14
Justin Davila <Jadavila9@gmail.com>
2025-09-10 23:34:16 -0400

    feat: use an iterator to yield result and call it in countdown function

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -3,6 +3,7 @@ package main
 import(
 	"fmt"
 	"io"
+	"iter"
 	"time"
 	"os"
 )
@@ -40,11 +41,21 @@ const sleep = "sleep"
 
 func Countdown(out io.Writer, sleeper Sleeper){
 	//using for loop to decrement and recreate a countdown
-	for i := countDownStart; i > 0; i-- {
+	for i := range countDownFrom(3){
 		fmt.Fprintln(out, i)
 		sleeper.Sleep()
 	}
 	fmt.Fprint(out, finalWord)
+}
+
+func countDownFrom(from int) iter.Seq[int] {
+	return func(yield func(int) bool) {
+		for i := from; i > 0; i -- {
+			if !yield(i) {
+				return
+			}
+		}
+	}
 }
 
 func (d *DefaultSleeper) Sleep() {
