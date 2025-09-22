c1dcce1bd82ce0cf6b8ed751bf1227ffe381e085
Justin Davila <Jadavila9@gmail.com>
2025-09-22 16:01:52 -0400

    feat: corrects test to use http response writer through spy store to acquire context

--- context/context_test.go
+++ context/context_test.go
@@ -2,6 +2,7 @@ package main
 
 import(
 	"context"
+	"errors"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -13,6 +14,11 @@ type SpyStore struct {
 	response string
 	t *testing.T
 }
+
+type SpyResponseWriter struct {
+	written bool
+}
+
 
 func(s *SpyStore) Fetch(ctx context.Context) (string, error) {
 	data := make(chan string, 1)
@@ -40,6 +46,19 @@ func(s *SpyStore) Fetch(ctx context.Context) (string, error) {
 	}
 }
 
+func (s *SpyResponseWriter) Header() http.Header {
+	s.written = true
+	return nil
+}
+
+func (s *SpyResponseWriter) Write([]byte) (int, error) {
+	s.written = true
+	return 0, errors.New("not implemented")
+}
+
+func (s *SpyResponseWriter) WriteHeader(statusCode int) {
+	s.written = true
+}
 
 func TestServer(t *testing.T) {
 	data := "hello, world"
@@ -55,23 +74,27 @@ func TestServer(t *testing.T) {
 		if response.Body.String() != data {
 			t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
 		}
-		})
-	//t.Run("tells store to cancel if request is cancelled", func(t *testing.T) {
-	//	store := &SpyStore{response: data, t: t}
-	//	svr := Server(store)
+	})
+	t.Run("tells store to cancel if request is cancelled", func(t *testing.T) {
+		store := &SpyStore{response: data, t: t}
+		svr := Server(store)
 
-	//	request := httptest.NewRequest(http.MethodGet, "/", nil)
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
 		
-	//	cancellingCtx, cancel := context.WithCancel(request.Context())
-	//	time.AfterFunc(5*time.Millisecond,cancel)
-	//	request = request.WithContext(cancellingCtx)
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond,cancel)
+		request = request.WithContext(cancellingCtx)
 
-	//	response := httptest.NewRecorder()
+		response := &SpyResponseWriter{}
 
-	//	svr.ServeHTTP(response, request)
+		svr.ServeHTTP(response, request)
 		
-	//	store.assertWasCancelled()
-	//})
+		if response.written { 
+			t.Error("a response should not have been written")
+		}
+
+	})
+	
 
 
 }
