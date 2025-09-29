b4b7c8b11694d96056722642111f15219ca0a5cc
Justin Davila <jadavila9@gmail.com>
2025-09-29 02:51:00 -0400

    feat: refactor covert to arabic function to use a for range over the roman numerals and building the string, then incrementing the arabic number by the value of the numerals and re assigning the roman value to the new string until the loop ends and returning the arabic value

--- propertyBased/numeral.go
+++ propertyBased/numeral.go
@@ -39,9 +39,13 @@ func ConvertToRoman(arabic int) string {
 }
 
 func ConvertToArabic(roman string) int {
-	total := 0
-	for range roman {
-		total++
+	arabic := 0
+
+	for _, numeral := range allRomanNumerals {
+		for strings.HasPrefix(roman, numeral.Symbol) {
+			arabic += numeral.Value
+			roman = strings.TrimPrefix(roman, numeral.Symbol)
+		}
 	}
-	return total
+	return arabic
 }
