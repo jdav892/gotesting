e6c8963dce441d9342f085321f31a05532fac48a
Justin Davila <jadavila9@gmail.com>
2025-11-18 01:09:53 -0500

    feat: add minute hand function to svgwriter

--- math/clockface/SVGWriter.go
+++ math/clockface/SVGWriter.go
@@ -7,6 +7,7 @@ import (
 )
 
 const (
+	minuteHandLength = 80
 	secondHandLength = 90
 	clockCentreX     = 150
 	clockCentreY     = 150
@@ -15,6 +16,7 @@ const (
 func SVGWriter(w io.Writer, t time.Time) {
 	io.WriteString(w, svgStart)
 	io.WriteString(w, bezel)
+	minuteHand(w, t)
 	secondHand(w, t)
 	io.WriteString(w, svgEnd)
 }
@@ -22,6 +24,14 @@ func SVGWriter(w io.Writer, t time.Time) {
 func secondHand(w io.Writer, t time.Time) {
 	p := secondHandPoint(t)
 	p = Point{p.X * secondHandLength, p.Y * secondHandLength} // scale
+	p = Point{p.X, -p.Y}                                      // flip
+	p = Point{p.X + clockCentreX, p.Y + clockCentreY}         // translate
+	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#f00;stroke-width:3px;"/>`, p.X, p.Y)
+}
+
+func minuteHand(w io.Writer, t time.Time) {
+	p := minuteHandPoint(t)
+	p = Point{p.X * minuteHandLength, p.Y * minuteHandLength} // scale
 	p = Point{p.X, -p.Y}                                      // flip
 	p = Point{p.X + clockCentreX, p.Y + clockCentreY}         // translate
 	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#f00;stroke-width:3px;"/>`, p.X, p.Y)
