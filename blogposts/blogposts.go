f146ca8571cdeee8fb51aafa2b700bf3dde05eb1
Justin Davila <jadavila9@gmail.com>
2025-12-06 18:09:35 -0500

    feat: refactor NewPostsFromFs

--- blogposts/blogposts.go
+++ blogposts/blogposts.go
@@ -1,11 +1,17 @@
 package blogposts
 
 import (
+	"io/fs"
 	"testing/fstest"
 )
 
 type Post struct{}
 
 func NewPostsFromFs(fileSystem fstest.MapFS) []Post {
-	return []Post{{}, {}}
+	dir, _ := fs.ReadDir(fileSystem, ".")
+	var posts []Post
+	for range dir {
+		posts = append(posts, Post{})
+	}
+	return posts
 }
--- blogposts/blogposts_test.go
+++ blogposts/blogposts_test.go
@@ -10,7 +10,7 @@ import (
 func TestNewBlogPosts(t *testing.T) {
 	fs := fstest.MapFS{
 		"hello world.md":  {Data: []byte("hi")},
-		"hello-world2.md": {Data: []byte("hi")},
+		"hello-world2.md": {Data: []byte("hola")},
 	}
 
 	posts := blogposts.NewPostsFromFs(fs)
