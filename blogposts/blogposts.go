6998254537cc6284d72f7db741a09641cbd766f7
Justin Davila <jadavila9@gmail.com>
2025-12-07 01:30:19 -0500

    feat: changed file reading to check for title string

--- blogposts/blogposts.go
+++ blogposts/blogposts.go
@@ -1,11 +1,14 @@
 package blogposts
 
 import (
+	"io"
 	"io/fs"
 	"testing/fstest"
 )
 
-type Post struct{}
+type Post struct {
+	Title string
+}
 
 func NewPostsFromFs(fileSystem fstest.MapFS) ([]Post, error) {
 	dir, err := fs.ReadDir(fileSystem, ".")
@@ -14,8 +17,28 @@ func NewPostsFromFs(fileSystem fstest.MapFS) ([]Post, error) {
 	}
 
 	var posts []Post
-	for range dir {
-		posts = append(posts, Post{})
+	for _, f := range dir {
+		post, err := getPost(fileSystem, f)
+		if err != nil {
+			return nil, err // need to clarify for total/single/ignore
+		}
+		posts = append(posts, post)
 	}
 	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
+	postFile, err := fileSystem.Open(f.Name())
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+
+	postData, err := io.ReadAll(postFile)
+	if err != nil {
+		return Post{}, err
+	}
+
+	post := Post{Title: string(postData)[7:]}
+	return post, nil
 }
--- blogposts/blogposts_test.go
+++ blogposts/blogposts_test.go
@@ -1,16 +1,24 @@
 package blogposts_test
 
 import (
+	"reflect"
 	"testing"
 	"testing/fstest"
 
 	blogposts "github.com/jdav892/gotesting/blogposts"
 )
 
+// type StubFailingFS struct{}
+//
+//_, err := blogposts.NewPostsFromFs, not sure where this goes yet
+// func (s StubFailingFS) Open(name string) (fs.File, error) {
+// 	return nil, errors.New("confirmed failure xd")
+// }
+
 func TestNewBlogPosts(t *testing.T) {
 	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("hi")},
-		"hello-world2.md": {Data: []byte("hola")},
+		"hello world.md":  {Data: []byte("Title: Post 1")},
+		"hello-world2.md": {Data: []byte("Title: Post 2")},
 	}
 
 	posts, err := blogposts.NewPostsFromFs(fs)
@@ -20,5 +28,12 @@ func TestNewBlogPosts(t *testing.T) {
 
 	if len(posts) != len(fs) {
 		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+	}
+
+	got := posts[0]
+	want := blogposts.Post{Title: "Post 1"}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
 	}
 }
