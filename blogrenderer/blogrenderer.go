a303a39a41ec2bb19e22a37568a2ec9ccbdfbe72
Justin Davila <jadavila9@gmail.com>
2026-01-18 03:11:52 -0500

    feat: passing test with newlines

--- blogrenderer/blogrenderer.go
+++ blogrenderer/blogrenderer.go
@@ -7,12 +7,32 @@ import (
 
 type Post struct {
 	Title       string
-	Body        string
 	Description string
+	Body        string
 	Tags        []string
 }
 
 func Render(w io.Writer, p Post) error {
-	_, err := fmt.Fprintf(w, "<h1>%s</h1>", p.Title)
-	return err
+	_, err := fmt.Fprintf(w, "<h1>%s</h1>\n<p>%s</p>\n", p.Title, p.Description)
+	if err != nil {
+		return err
+	}
+
+	_, err = fmt.Fprintf(w, "Tags: <ul>")
+	if err != nil {
+		return err
+	}
+
+	for _, tag := range p.Tags {
+		_, err = fmt.Fprintf(w, "<li>%s</li>", tag)
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = fmt.Fprintf(w, "</ul>")
+	if err != nil {
+		return err
+	}
+	return nil
 }
