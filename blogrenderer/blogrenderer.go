7457a1fa3ad65119ff432bbc4864fcb1cf9a399b
Justin Davila <jadavila9@gmail.com>
2026-01-19 03:13:18 -0500

    WIP: need to pass functions to templates

--- blogrenderer/blogrenderer.go
+++ blogrenderer/blogrenderer.go
@@ -13,19 +13,42 @@ type Post struct {
 	Tags        []string
 }
 
+type PostRenderer struct {
+	templ *template.Template
+}
+
 var (
 	//go:embed templates/*
 	postTemplates embed.FS
 )
 
-func Render(w io.Writer, p Post) error {
+func NewPostRenderer()(*PostRenderer, error) {
 	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostRenderer{templ: templ}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+
+	if err := r.templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
+	indexTemplate := `<ol>{{range .}}<li><a href="/post/{{.Title}}">{{.Title}}</a></li>{{end}}</ol>`
+	templ, err := template.New("index").Parse(indexTemplate)
 	if err != nil {
 		return err
 	}
 
-	if err := templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
+	if err := templ.Execute(w, posts); err != nil {
 		return err
 	}
+
 	return nil
 }
--- blogrenderer/blogrenderer_test.go
+++ blogrenderer/blogrenderer_test.go
@@ -10,22 +10,45 @@ import (
 )
 
 func TestRender(t *testing.T) {
-	aPost := blogrenderer.Post{
-		Title:       "hello world",
-		Body:        "This is a post",
-		Description: "This is a description",
-		Tags:        []string{"go", "tdd"},
+	var(
+		aPost = blogrenderer.Post{
+				Title:       "hello world",
+				Body:        "This is a post",
+				Description: "This is a description",
+				Tags:        []string{"go", "tdd"},
+			}
+	)
+	
+	postRenderer, err := blogrenderer.NewPostRenderer()
+
+	if err != nil {
+		t.Fatal(err)
 	}
 
 	t.Run("it converts a single post into HTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
 
-		err := blogrenderer.Render(&buf, aPost)
-		if err != nil {
+		if err := postRenderer.Render(&buf, aPost); err != nil {
 			t.Fatal(err)
 		}
 
 		approvals.VerifyString(t, buf.String())
+	})
+
+	t.Run("it renders an index of posts", func(t *testing.T) {
+		buf := bytes.Buffer{}
+		posts := []blogrenderer.Post{{Title: "Hello World"}, {Title: "Hello World 2"}}
+
+		if err := postRenderer.RenderIndex(&buf, posts); err != nil {
+			t.Fatal(err)
+		}
+
+		got := buf.String()
+		want := `<ol><li><a href="/post/hello-world">Hello World</a></li><li><a href="post/hello-world-2">Hello World 2</a></li><ol>`
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	})
 }
 
@@ -38,8 +61,14 @@ func BenchmarkRender(b *testing.B) {
 			Tags: []string{"go", "tdd"},
 		}
 	)
+	
+	postRenderer, err := blogrenderer.NewPostRenderer()
+	
+	if err != nil {
+		b.Fatal(err)
+	}
 
 	for b.Loop(){
-		blogrenderer.Render(io.Discard, aPost)
+		postRenderer.Render(io.Discard, aPost)
 	}
 }
