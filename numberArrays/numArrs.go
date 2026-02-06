13fb835ecc5d3493b5fde26bb6cb9ea65eac7df6
Justin Davila <jadavila9@gmail.com>
2026-02-06 01:57:07 -0500

    feat: refactor sum functions to use reduce

--- numberArrays/numArrs.go
+++ numberArrays/numArrs.go
@@ -2,26 +2,21 @@ package numberarrays
 
 // Sum calculates the total from a slice of numbers
 func Sum(numbers []int) int {
-	var sum int
-	for _, number := range numbers {
-		sum += number
-	}
-	return sum
+	add := func(acc, x int) int { return acc + x }
+	return Reduce(numbers, add, 0)
 }
 
 // SumAllTails calculates the sum of all but the first number given
-func SumAllStails(numbersToSum ...[]int) []int {
-	var sums []int
-	for _, numbers := range numbersToSum {
-		if len(numbers) == 0 {
-			sums = append(sums, 0)
+func SumAllStails(numbers ...[]int) []int {
+	sumTail := func(acc, x []int) []int {
+		if len(x) == 0 {
+			return append(acc, 0)
 		} else {
-			tail := numbers[1:]
-			sums = append(sums, Sum(tail))
+			tail := x[1:]
+			return append(acc, Sum(tail))
 		}
 	}
-
-	return sums
+	return Reduce(numbers, sumTail, []int{})
 }
 
 func Reduce[T any](collection []T, f func(T, T) T, initialValue T) T {
