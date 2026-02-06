b38068c1ac3b7c10cffb4284abdc321784fb8059
Justin Davila <jadavila9@gmail.com>
2026-02-06 02:04:43 -0500

    feat: tests for fold sum functions

---
+++ numberArrays/numArrs_test.go
@@ -0,0 +1,30 @@
+package numberarrays
+
+import (
+	"testing"
+)
+
+func AssertEqual(t *testing.T, got, want interface{}) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func TestReduce(t *testing.T) {
+	t.Run("multiplication of all elements", func(t *testing.T) {
+		multiply := func(x, y int) int {
+			return x * y
+		}
+
+		AssertEqual(t, Reduce([]int{1, 2, 3}, multiply, 1), 6)
+	})
+
+	t.Run("concatenate strings", func(t *testing.T) {
+		concatenate := func(x, y string) string {
+			return x + y
+		}
+
+		AssertEqual(t, Reduce([]string{"a", "b", "c"}, concatenate, ""), "abc")
+	})
+}
